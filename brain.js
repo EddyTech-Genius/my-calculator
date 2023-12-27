dfc76b2f2dc63e0547b0a171d762f5c445d040bc
edwin2011-la <edwinakobeh789@gmail.com>
2023-12-27 11:51:24 -0800

    first commit

---
+++ brain.js
@@ -0,0 +1,12 @@
+let display = document.querySelector(".display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+
+buttons.map( button => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
