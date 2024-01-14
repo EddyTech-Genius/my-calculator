21d4e417b6bd43d266faf4f9d08f558b377bf376
edwin2011-la <edwinakobeh789@gmail.com>
2024-01-13 17:48:53 -0800

    second save

--- brain.js
+++ brain.js
@@ -5,8 +5,17 @@ let buttons = Array.from(document.getElementsByClassName("button"));
 buttons.map( button => {
   button.addEventListener("click", (e) => {
     switch (e.target.innerText) {
+      case "C":
+        display.innerText = "";
+        break
       default:
         display.innerText += e.target.innerText;
     }
   });
 });
+
+
+
+
+
+
